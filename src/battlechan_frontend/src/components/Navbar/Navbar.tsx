defba38f19a56730bda4a96f8936a6bc92d89632
Shivam Gupta <tsg782@gmail.com>
2024-03-09 17:43:44 +0530

    updated-tailwind

---
+++ src/battlechan_frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import bclogo from "../../../images/battlechan 1.png"
+export interface IAppProps {
+}
+
+export default class App extends React.PureComponent<IAppProps> {
+  public render() {
+    return (
+      <div className=' bg-[#0A0A0A]  text-white       ' >
+        <div ><img src={bclogo} alt="" /></div>
+        <div className='p-2 m-2 rounded-sm'>
+            <input type="text" placeholder='Search here...' />
+        </div>
+        <div className=' border-solid-1 '>
+            <div >1 $Time Token: $0.0050</div>
+            <div className='bg-white text-black p-2 m-2' >Buy</div>
+        </div>
+
+
+      </div>
+    );
+  }
+}
