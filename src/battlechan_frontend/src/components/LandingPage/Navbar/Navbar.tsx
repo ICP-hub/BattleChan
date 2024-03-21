2571e28e53ddf3565cd37a0ba9c926a2fcdd2f13
Shivam Gupta <tsg782@gmail.com>
2024-03-21 18:01:27 +0530

    auth-dn

--- src/battlechan_frontend/src/components/LandingPage/Navbar/Navbar.tsx
+++ src/battlechan_frontend/src/components/LandingPage/Navbar/Navbar.tsx
@@ -1,65 +1,44 @@
 import React from "react";
 
-import { ConnectButton, ConnectDialog } from "@connect2ic/react";
+import { useSelector } from "react-redux";
 
 import { MdArrowOutward } from "react-icons/md";
 import darkLogo from "../../../images/dark_logo.png";
 import lightLogo from "../../../images/light_logo.png";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
-import { useSelector } from "react-redux";
 
-// type Theme = {
-//   darkColor: string;
-//   lightColor: string;
-//   handleThemeSwitch: any;
-// };
+// Assuming @connect2ic/react components are used elsewhere or will be used.
+// import { ConnectButton, ConnectDialog } from "@connect2ic/react";
 
 type Theme = {
   darkColor: string;
   lightColor: string;
   handleThemeSwitch: any;
-  setModalOpen: (open: boolean) => void; // Include setModalOpen in the Theme type
+  setModalOpen: (open: boolean) => void;
 };
 
-const Navbar = (props: Theme,) => {
+const Navbar = (props: Theme) => {
+  const principal = useSelector((state : any) => state.internet.principal);
 
-
-  // const principal = useSelector((currState) => currState.internet.principal);
-  const principal = useSelector((state: RootState) => state.internet.principal);
-
-
-  const darkColor = props.darkColor;
-  const lightColor = props.lightColor;
+  const { darkColor, lightColor, setModalOpen } = props;
   const className = "LandingPage__Navbar";
 
-  // const manageHandler = () => {
-  //   !principal ? setModalOpen(true) : setModalOpen(false);
-  // };
   const manageHandler = () => {
-    !principal ? props.setModalOpen(true) : props.setModalOpen(false);
+    !principal ? setModalOpen(true) : setModalOpen(false);
   };
-
 
   return (
     <nav
-      className={
-        className +
-        ` flex-row-center justify-between bg-${darkColor} text-${lightColor}` +
-        " laptop:py-8 laptop:px-16 px-8 py-8"
-      }
+      className={`${className} flex-row-center justify-between bg-${darkColor} text-${lightColor} laptop:py-8 laptop:px-16 px-8 py-8`}
     >
       <img
         src={darkColor.includes("dark") ? darkLogo : lightLogo}
         alt="BATTLE CHAN"
-        className={className + "__logo w-28 object-contain pointer-events-none"}
+        className={`${className}__logo w-28 object-contain pointer-events-none`}
       />
 
       <section
-        className={
-          className +
-          "__rightSection flex-row-center font-bold" +
-          " laptop:gap-4 gap-2"
-        }
+        className={`${className}__rightSection flex-row-center font-bold laptop:gap-4 gap-2`}
       >
         <ThemeSwitch
           handleThemeSwitch={props.handleThemeSwitch}
@@ -68,45 +47,27 @@ const Navbar = (props: Theme,) => {
         />
 
         <div
-          className={
-            className +
-            `__timeToken text-${lightColor} gap-2 flex-row-center border border-green rounded-[3rem]` +
-            "  laptop:p-1.5 laptop:pl-6 p-1.5 pl-4 text-base"
-          }
+          className={`${className}__timeToken text-${lightColor} gap-2 flex-row-center border border-green rounded-[3rem] laptop:p-1.5 laptop:pl-6 p-1.5 pl-4 text-base`}
         >
           1 Time Token:
-          <span className={className + "__timeToken__amount text-light-green"}>
+          <span className={`${className}__timeToken__amount text-light-green`}>
             $0.0050
           </span>
           <button
-            className={
-              className +
-              `__timeToken__butButton small-button bg-${lightColor} text-${darkColor}`
-            }
+            className={`${className}__timeToken__butButton small-button bg-${lightColor} text-${darkColor}`}
           >
             Buy
           </button>
         </div>
-        {/* <button onClick={manageHandler}>connect wallet</button>
-        <button
-          className={
-            className + "__connectWalletBtn flex-row-center green-button"
-          }
-        > */}
-        <button onClick={manageHandler}>connect wallet</button>
-      <button
-        className={
-          className + "__connectWalletBtn flex-row-center green-button"
-        }
-      >
-          {/* <ConnectButton />  */}
+        
+        <button onClick={manageHandler} className={`${className}__connectWalletBtn flex-row-center green-button`}>
           <MdArrowOutward />
+          connect wallet
         </button>
-
-        {/* <ConnectDialog /> */}
       </section>
     </nav>
   );
 };
 
 export default Navbar;
+
