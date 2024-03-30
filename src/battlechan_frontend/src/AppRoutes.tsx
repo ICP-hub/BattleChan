e6ad04efd9e64050d6edcf25bb12bd97381c62f6
Neha Kumari <neha.kumari@quadbtech.com>
2024-03-30 15:30:24 +0530

    Dasboard body fixed

--- src/battlechan_frontend/src/AppRoutes.tsx
+++ src/battlechan_frontend/src/AppRoutes.tsx
@@ -1,100 +1,91 @@
 // AppRoutes.tsx
-import React, { Suspense, lazy, useEffect, useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Loader from './components/Loader/Loader';
-import { useConnect } from '@connect2ic/react';
+import React, { Suspense, lazy, useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Loader from "./components/Loader/Loader";
+import { useConnect } from "@connect2ic/react";
 
 // Lazy load pages
-const Landing = React.lazy(() => import('./pages/Landing/Landing'));
-const DashboardRoutes = React.lazy(() => import('./pages/Routes'));
-const UserSetting = React.lazy(() => import('../src/components/Dashboard/SettingProfile/SettingProfile'))
-const UserProfile = React.lazy(() => import('../src/components/Dashboard/UserProfile/UserProfile'))
-
+const Landing = React.lazy(() => import("./pages/Landing/Landing"));
+const DashboardRoutes = React.lazy(() => import("./pages/Routes"));
+const UserSetting = React.lazy(
+  () => import("../src/components/Dashboard/SettingProfile/SettingProfile")
+);
+const UserProfile = React.lazy(
+  () => import("../src/components/Dashboard/UserProfile/UserProfile")
+);
 
 interface AppRoutesProps {
-    
-    handleThemeSwitch: () => void; // Adjust based on the actual implementation
+  handleThemeSwitch: () => void; // Adjust based on the actual implementation
 }
 
-const AppRoutes:React.FC<AppRoutesProps> = ({ handleThemeSwitch }) => {
-    let { isConnected, principal , isIdle  , isInitializing} = useConnect()
-    const [allow, setAllow] = useState<null | boolean>(null);
+const AppRoutes: React.FC<AppRoutesProps> = ({ handleThemeSwitch }) => {
+  let { isConnected, principal, isIdle, isInitializing } = useConnect();
+  const [allow, setAllow] = useState<null | boolean>(null);
 
-    useEffect(() => {
-         if(isInitializing == false ){
-             setAllow(principal ? true : false);
-         }
-      console.log("intitilizing is " ,isInitializing)
+  useEffect(() => {
+    if (isInitializing == false) {
+      setAllow(principal ? true : false);
+    }
+    console.log("intitilizing is ", isInitializing);
+  }, [principal, isInitializing]);
 
-      }, [principal , isInitializing  ]);
+  console.log("allow is ", allow);
+  console.log("principal is ", principal);
 
-      console.log("allow is " ,allow)
-      console.log("principal is " ,principal)
+  let x = "a";
 
-
-
-    let x  = "a"
-
-
-      
-    
-    React.useEffect(() => {
-        if (principal) {
-            console.log('Principalm eff of aepr :', principal);
-            // Perform any other actions that depend on the updated principal value
+  React.useEffect(() => {
+    if (principal) {
+      console.log("Principalm eff of aepr :", principal);
+      // Perform any other actions that depend on the updated principal value
+    }
+  }, [principal]);
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Landing handleThemeSwitch={handleThemeSwitch} />
+          </Suspense>
         }
-    }, [principal]); 
-    return (
-        <Routes>
-            <Route
-                path="/"
-                element={
-                    <Suspense fallback={<Loader />}>
-                        <Landing handleThemeSwitch={handleThemeSwitch} />
-                    </Suspense>
-                }
-            />
+      />
 
-            <Route
-                path="/dashboard/"
-                element={
-                   
-                        <Suspense fallback={<Loader />}>
-                            <DashboardRoutes handleThemeSwitch={handleThemeSwitch} />
-                        </Suspense>
-                }
-            />
-            <Route
-                path="/dashboard/settingProfile/*"
-                element={
-                    allow == true ? (
-                        <Suspense fallback={<Loader />}>
-                            <UserSetting handleThemeSwitch={handleThemeSwitch}/>
-                        </Suspense>
-                    ) : allow == false ?(
-
-                        <Navigate to="/" />
-                    ) : null
-                }
-            />
-            <Route
-                path="/dashboard/UserProfile/*"
-                element={
-                    allow == true ? (
-                        <Suspense fallback={<Loader />}>
-                            <UserProfile handleThemeSwitch={handleThemeSwitch}/>
-                        </Suspense>
-                    ) : allow == false ?(
-
-                        <Navigate to="/" />
-                    ) : null
-                }
-            />
-
-        </Routes>
-
-    );
+      <Route
+        path="/dashboard/"
+        element={
+          <Suspense fallback={<Loader />}>
+            <DashboardRoutes handleThemeSwitch={handleThemeSwitch} />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/dashboard/settingProfile/*"
+        element={
+          allow == true ? (
+            <Suspense fallback={<Loader />}>
+              <UserSetting handleThemeSwitch={handleThemeSwitch} />
+            </Suspense>
+          ) : allow == false ? (
+            <Navigate to="/" />
+          ) : null
+        }
+      />
+      <Route
+        path="/dashboard/UserProfile/*"
+        element={
+          allow == true ? (
+            <Suspense fallback={<Loader />}>
+              <UserProfile handleThemeSwitch={handleThemeSwitch} />
+            </Suspense>
+          ) : allow == false ? (
+            <Navigate to="/" />
+          ) : null
+        }
+      />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
