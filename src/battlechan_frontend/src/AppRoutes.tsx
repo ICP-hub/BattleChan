92eeb77f07f1bc70460a57b39be8768cb88d8c64
Khushali <khushali.trivedi@quadbtech.com>
2024-05-16 18:30:15 +0530

    change

--- src/battlechan_frontend/src/AppRoutes.tsx
+++ src/battlechan_frontend/src/AppRoutes.tsx
@@ -96,13 +96,13 @@ const AppRoutes: React.FC<AppRoutesProps> = ({ handleThemeSwitch }) => {
       <Route
         path="/dashboard/analytics/*"
         element={
-          // allow == true ? (
+          allow == true ? (
             <Suspense fallback={<Loader />}>
               <Analytics handleThemeSwitch={handleThemeSwitch} />
             </Suspense>
-          // ) : allow == false ? (
-          //   <Navigate to="/" />
-          // ) : null
+          ) : allow == false ? (
+            <Navigate to="/" />
+          ) : null
         }
       />
       <Route
