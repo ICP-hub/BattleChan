0c8611020ad6f08abe9bead7f1db10c9c1fcc771
DiptanshuG <diptanshubhawsar50@gmail.com>
2024-05-23 13:17:13 +0530

    update

--- src/battlechan_frontend/src/Layout.tsx
+++ src/battlechan_frontend/src/Layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import Navbar from "./components/LandingPage/Navbar/Navbar";
+import Navbar from "./components/Dashboard/Navbar/Navbar";
+import NavbarLanding from "./components/LandingPage/Navbar/Navbar";
+import { useLocation } from "react-router-dom";
+
 
 interface LayoutProps {
   handleThemeSwitch: () => void;
@@ -7,10 +10,15 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, handleThemeSwitch }) => {
+  const location = useLocation();
+
   return (
     <div>
-      <Navbar handleThemeSwitch={handleThemeSwitch} />
-      {children} 
+ {location.pathname === "/" ? (
+        <NavbarLanding handleThemeSwitch={handleThemeSwitch} />
+      ) : (
+        <Navbar handleThemeSwitch={handleThemeSwitch} />
+      )}      {children} 
     </div>
   );
 };
